070b46ff32646a3587288ba2af80e27190e8b1f0
Yerokkim <kimyerok@gmail.com>
2021-09-24 17:29:38 +0200

    create business logic for getting movie

---
+++ lib/application/movie/get_movie_state_notifier.dart
@@ -0,0 +1,29 @@
+import 'package:netflix_clone/domain/movie/i_movie_repositor.dart';
+import 'package:netflix_clone/domain/movie/movie.dart';
+import 'package:netflix_clone/domain/movie/movie_result.dart';
+
+import 'package:riverpod/riverpod.dart';
+
+class GetMovieStateNotifier extends StateNotifier<AsyncValue<List<MovieResult>>> {
+  final Reader _reader;
+
+  GetMovieStateNotifier(this._reader) : super(const AsyncValue.loading()) {
+    getMovies();
+  }
+
+  Future<AsyncValue> getMovies() async {
+    try {
+      final _movieRepository = _reader(movieRepositoryProvider);
+
+      final movies = await _movieRepository.getMovies();
+
+      if (movies?.results != null) {
+        return state = AsyncValue.data(movies!.results!);
+      } else {
+        return state = state = AsyncValue.error('We can not fetch the movie!');
+      }
+    } catch (e) {
+      return state = AsyncValue.error(e);
+    }
+  }
+}
