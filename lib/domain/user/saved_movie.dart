ae4784ce2be02ecef0df7667f25235a33f15a513
Yerokkim <kimyerok@gmail.com>
2021-09-24 17:31:09 +0200

    create domain for the save movie

---
+++ lib/domain/user/saved_movie.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:netflix_clone/domain/movie/movie_result.dart';
+
+abstract class SavedMovie {
+  @JsonKey(required: true)
+  final String userId;
+
+  final MovieResult movie;
+
+  @JsonKey()
+  final DateTime? createdAt;
+
+  @JsonKey(defaultValue: true)
+  bool? isAdded;
+
+  SavedMovie({
+    required this.userId,
+    required this.movie,
+    this.isAdded,
+    this.createdAt,
+  });
+
+  dynamic toJson();
+}
