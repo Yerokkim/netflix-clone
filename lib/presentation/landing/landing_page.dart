908e058dac6b6c903a8f8f18821575dc335bb3a7
Yerokkim <kimyerok@gmail.com>
2021-09-26 21:59:36 +0200

    basic ui created for the action buttons

--- lib/presentation/landing/landing_page.dart
+++ lib/presentation/landing/landing_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:netflix_clone/domain/movie/movie_result.dart';
 import 'package:netflix_clone/infrastructure/database/external_api_service.dart';
+import 'package:netflix_clone/infrastructure/models/movie_result.dart';
 import 'package:netflix_clone/presentation/loading/loading_page.dart';
 import 'package:netflix_clone/presentation/providers/movie_provider/movie_providers.dart';
 import 'package:netflix_clone/presentation/wigets/action_button_list.dart';
+// import 'package:provider/provider.dart';
 
 class LandingPage extends StatelessWidget {
   @override
@@ -25,7 +27,7 @@ class LandingPage extends StatelessWidget {
 }
 
 class MovieGridView extends StatelessWidget {
-  final List<MovieResult> movieData;
+  final List<MovieResultModel> movieData;
 
   MovieGridView({required this.movieData});
 
@@ -39,44 +41,99 @@ class MovieGridView extends StatelessWidget {
             SliverList(
               delegate: SliverChildListDelegate(
                 ([
-                  Stack(
-                    children: [
-                      Card(
-                        semanticContainer: true,
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        child: Stack(children: <Widget>[
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: Image.network(
-                              ApiService.movieImageApi(imageId: movieData[0].backdropPath),
-                              // width: 140,
-                              height: 800,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 10,
-                            child: Column(
+                  Card(
+                    semanticContainer: true,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Stack(children: <Widget>[
+                      AspectRatio(
+                        aspectRatio: 1,
+                        child: Image.network(
+                          ApiService.movieImageApi(imageId: movieData[0].backdropPath),
+                          height: 800,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: BackgroundGradient(),
+                      ),
+                      Positioned(
+                        bottom: 20,
+                        right: 45,
+                        child: Column(
+                          children: [
+                            Row(
                               children: [
-                                Text(movieData[0].name!,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 30.0,
-                                      fontWeight: FontWeight.bold,
-                                    )),
+                                Image.asset(
+                                  'assets/images/logo_small.png',
+                                  fit: BoxFit.contain,
+                                  width: 30,
+                                  height: 30,
+                                ),
+                                Text(
+                                  'SERIE',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ],
                             ),
-                          ),
-                          Positioned.fill(
-                            child: BackgroundGradient(),
-                          ),
-                        ]),
+                            Text(
+                              movieData[0].name!,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
+                    ]),
                   ),
-                  ActionButtonList(),
+                  Consumer(
+                    builder: (BuildContext context, T Function<T>(ProviderBase<Object?, T>) watch,
+                        Widget? child) {
+                      final toggleActionState =
+                          watch(addToListStateNotifierProvider(movieData[0].id.toString()));
+                      final toggleActionProvider = watch(
+                          addToListStateNotifierProvider(movieData[0].id.toString()).notifier);
+
+                      print(toggleActionState);
+                      return toggleActionState.when(
+                        unClick: () => ActionButtonList(
+                          isFirstActionBtnClicked: false,
+                          firstActionButtonPressed: () {
+                            toggleActionProvider.toggleAddToList(movie: movieData[0]);
+                          },
+                        ),
+                        clicked: (id) => ActionButtonList(
+                          isFirstActionBtnClicked: true,
+                          firstActionButtonPressed: () {
+                            toggleActionProvider.toggleAddToList(
+                                movie: movieData[0], movieDocumentId: id);
+                          },
+                        ),
+                        toggleFailed: (e) => Text(''),
+                      );
+                    },
+                  ),
                   SizedBox(
                     height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                    ),
+                    child: Text(
+                      'Top Trend',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                   Container(
                       padding: const EdgeInsets.all(8.0),
@@ -99,16 +156,6 @@ class MovieGridView extends StatelessWidget {
                               ),
                             ]),
                           );
-
-                          //  Card(
-
-                          //   child: AspectRatio(
-                          //       aspectRatio: 1,
-                          //       child: Image.network(
-                          //         ApiService.movieImageApi(imageId: movieData[index].posterPath),
-                          //         fit: BoxFit.cover,
-                          //       )),
-                          // );
                         },
                       )),
                 ]),
@@ -136,7 +183,7 @@ class BackgroundGradient extends StatelessWidget {
             0.8,
           ],
           colors: [
-            Colors.black.withOpacity(0.7),
+            Colors.black.withOpacity(0.8),
             Colors.black.withOpacity(0.1),
           ],
         ),
--- lib/presentation/providers/movie_provider/movie_providers.dart
+++ lib/presentation/providers/movie_provider/movie_providers.dart
@@ -1,5 +1,10 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:netflix_clone/application/movie/add_to_list/add_to_list_state_notifier.dart';
 import 'package:netflix_clone/application/movie/get_movie_state_notifier.dart';
 
 final movieStateNotifierProvider = StateNotifierProvider<GetMovieStateNotifier, AsyncValue>(
     (ref) => GetMovieStateNotifier(ref.read));
+
+final addToListStateNotifierProvider =
+    StateNotifierProvider.family<AddToListStateNotifier, AddToListState, String>(
+        (ref, movieId) => AddToListStateNotifier(ref.read, movieId: movieId));
