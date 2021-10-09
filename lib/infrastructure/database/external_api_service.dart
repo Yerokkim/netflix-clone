3d32a48757376ffa21de47e231dce9b601990618
Yerokkim <kimyerok@gmail.com>
2021-10-09 20:45:13 +0200

    add series api

--- lib/infrastructure/database/external_api_service.dart
+++ lib/infrastructure/database/external_api_service.dart
@@ -1,6 +1,9 @@
 class ApiService {
-  static String popularMovieApi({required apiKey}) =>
+  static String popularSeriesApi({required apiKey}) =>
       'https://api.themoviedb.org/3/tv/popular?api_key=$apiKey&language=en-US&page=1';
+
+  static String popularMovieApi({required apiKey}) =>
+      'https://api.themoviedb.org/3/movie/popular?api_key=$apiKey&language=en-US&page=1';
 
   static String movieImageApi({required imageId}) => 'https://image.tmdb.org/t/p/original/$imageId';
 }
