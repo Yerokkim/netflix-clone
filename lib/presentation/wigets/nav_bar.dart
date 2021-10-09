a3e0ea4def3d75d6929e6501d5494a4bae4d1da8
Yerokkim <kimyerok@gmail.com>
2021-10-09 20:43:21 +0200

    add search icon to the nav

--- lib/presentation/wigets/nav_bar.dart
+++ lib/presentation/wigets/nav_bar.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/presentation/custom_search/custom_search.dart';
 
 class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.black,
+      actions: [
+        IconButton(
+          onPressed: () {
+            showSearch(
+              context: context,
+              delegate: CustomSearchDelegate(),
+            );
+          },
+          icon: Icon(Icons.search),
+        ),
+      ],
       title: Container(
         child: Row(
           children: [
