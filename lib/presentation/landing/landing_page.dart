457437d93b48dd67207f00c3a3ec9946c0e80de9
Yerokkim <kimyerok@gmail.com>
2021-10-09 20:44:09 +0200

    adding cacheNetworkImage

--- lib/presentation/landing/landing_page.dart
+++ lib/presentation/landing/landing_page.dart
@@ -1,35 +1,36 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:netflix_clone/domain/movie/movie_result.dart';
 import 'package:netflix_clone/infrastructure/database/external_api_service.dart';
 import 'package:netflix_clone/infrastructure/models/movie_result.dart';
+import 'package:netflix_clone/presentation/content_info_detail/content_info_detail_page.dart';
 import 'package:netflix_clone/presentation/loading/loading_page.dart';
 import 'package:netflix_clone/presentation/providers/movie_provider/movie_providers.dart';
 import 'package:netflix_clone/presentation/wigets/action_button_list.dart';
 // import 'package:provider/provider.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return Consumer(builder: (context, watch, child) {
-      // watch(movieStateNotifierProvider.notifier).getMovies();
-      final _movieState = watch(movieStateNotifierProvider);
+  Widget build(BuildContext context, ScopedReader watch) {
+    final _seriesStream = watch(getAllSeriesProvider);
 
-      return _movieState.when(
-        data: (movies) => MovieGridView(
-          movieData: movies,
-        ),
+    return _seriesStream.when(
+        data: (movies) => MovieGridView(seriesData: movies),
         loading: () => LoadingPage(),
-        error: (e, _) => Text('error'),
-      );
-    });
+        error: (error, _) {
+          return Text('error');
+        });
   }
 }
 
 class MovieGridView extends StatelessWidget {
-  final List<MovieResultModel> movieData;
+  final List<MovieResult?> seriesData;
 
-  MovieGridView({required this.movieData});
+  MovieGridView({
+    Key? key,
+    required this.seriesData,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +38,7 @@ class MovieGridView extends StatelessWidget {
       backgroundColor: Colors.black,
       body: SafeArea(
         child: CustomScrollView(
+          shrinkWrap: true,
           slivers: [
             SliverList(
               delegate: SliverChildListDelegate(
@@ -44,21 +46,20 @@ class MovieGridView extends StatelessWidget {
                   Card(
                     semanticContainer: true,
                     clipBehavior: Clip.antiAliasWithSaveLayer,
-                    child: Stack(children: <Widget>[
-                      AspectRatio(
-                        aspectRatio: 1,
-                        child: Image.network(
-                          ApiService.movieImageApi(imageId: movieData[0].backdropPath),
-                          height: 800,
-                          fit: BoxFit.cover,
+                    child: Stack(alignment: Alignment.center, children: <Widget>[
+                      Positioned(
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                ApiService.movieImageApi(imageId: seriesData[0]!.backdropPath),
+                            height: 800,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                      Positioned.fill(
-                        child: BackgroundGradient(),
-                      ),
                       Positioned(
-                        bottom: 20,
-                        right: 45,
+                        bottom: 14,
                         child: Column(
                           children: [
                             Row(
@@ -80,7 +81,7 @@ class MovieGridView extends StatelessWidget {
                               ],
                             ),
                             Text(
-                              movieData[0].name!,
+                              seriesData[0]!.name!,
                               style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 40.0,
@@ -90,30 +91,33 @@ class MovieGridView extends StatelessWidget {
                           ],
                         ),
                       ),
+                      // Positioned.fill(
+                      //   child: BackgroundGradient(),
+                      // ),
                     ]),
                   ),
                   Consumer(
                     builder: (BuildContext context, T Function<T>(ProviderBase<Object?, T>) watch,
                         Widget? child) {
                       final toggleActionState =
-                          watch(addToListStateNotifierProvider(movieData[0].id.toString()));
+                          watch(addToListStateNotifierProvider(seriesData[0]!.id.toString()));
                       final toggleActionProvider = watch(
-                          addToListStateNotifierProvider(movieData[0].id.toString()).notifier);
+                          addToListStateNotifierProvider(seriesData[0]!.id.toString()).notifier);
 
-                      print(toggleActionState);
                       return toggleActionState.when(
                         unClick: () => ActionButtonList(
                           isFirstActionBtnClicked: false,
                           firstActionButtonPressed: () {
-                            toggleActionProvider.toggleAddToList(movie: movieData[0]);
+                            toggleActionProvider.toggleAddToList(movie: seriesData[0]!);
                           },
                         ),
                         clicked: (id) => ActionButtonList(
                           isFirstActionBtnClicked: true,
                           firstActionButtonPressed: () {
                             toggleActionProvider.toggleAddToList(
-                                movie: movieData[0], movieDocumentId: id);
+                                movie: seriesData[0]!, movieDocumentId: id);
                           },
+                          secondActionButtonPressed: () {},
                         ),
                         toggleFailed: (e) => Text(''),
                       );
@@ -135,29 +139,30 @@ class MovieGridView extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Container(
-                      padding: const EdgeInsets.all(8.0),
-                      margin: const EdgeInsets.symmetric(vertical: 20.0),
-                      height: 250.0,
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: movieData.length - 1,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            semanticContainer: true,
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            child: Stack(children: <Widget>[
-                              Image.network(
-                                ApiService.movieImageApi(imageId: movieData[index + 1].posterPath),
-                                width: 140,
-                                height: 250,
-                                fit: BoxFit.cover,
-                              ),
-                            ]),
-                          );
-                        },
-                      )),
+                  MovieGridWidget(movieData: seriesData),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                    ),
+                    child: Text(
+                      'Top Trend',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Consumer(builder: (context, watch, child) {
+                    final _movieStream = watch(getAllMovieProvider);
+                    return _movieStream.when(
+                        data: (movie) => MovieGridWidget(movieData: movie),
+                        loading: () => LoadingPage(),
+                        error: (error, _) => Text('error'));
+                  }),
                 ]),
               ),
             ),
@@ -165,6 +170,54 @@ class MovieGridView extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class MovieGridWidget extends StatelessWidget {
+  const MovieGridWidget({
+    Key? key,
+    required this.movieData,
+  }) : super(key: key);
+
+  final List<MovieResult?> movieData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(vertical: 20.0),
+        height: 250.0,
+        child: ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: movieData.length - 1,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ContentInfoDetailPage(
+                      movieData: movieData[index + 1]!,
+                    ),
+                  ),
+                );
+              },
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Stack(children: <Widget>[
+                  CachedNetworkImage(
+                    imageUrl: ApiService.movieImageApi(imageId: movieData[index + 1]?.posterPath),
+                    width: 140,
+                    height: 250,
+                    fit: BoxFit.cover,
+                  ),
+                ]),
+              ),
+            );
+          },
+        ));
   }
 }
 
